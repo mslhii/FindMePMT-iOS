f0091271fbc3677e4c2eba85937833e11e0f1596
Michael Hii <mslhii@yahoo.com>
2015-09-28 01:06:21 -0700

    Refactor to make it my project for modifications again

--- FindMePMT/FindMePMT-Bridging-Header.h
+++ FindMePMT/FindMePMT-Bridging-Header.h
@@ -1,4 +1,12 @@
 //
+//  FindMePMT-Bridging-Header.h
+//  Find Me PMT
+//
+//  Created by Michael Hii on 9/28/15.
+//  Copyright © 2015 Kritikal Error Apps. All rights reserved.
+//
+
+//
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
